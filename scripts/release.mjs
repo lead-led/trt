ac3f2db89de0c1efb9b5f9c7f56ec6fa79c703f2
dlamon1 <devon.lamond@gmail.com>
2025-10-11 07:23:56 -0700

    Update package.json version and scripts; add release script and new icons

---
+++ scripts/release.mjs
@@ -0,0 +1,19 @@
+import 'dotenv/config.js';
+import { spawn } from 'node:child_process';
+
+const args = [
+  '--config',
+  'electron-builder.json',
+  '--publish',
+  'always',
+  '-mwl',
+];
+
+const child = spawn('electron-builder', args, {
+  stdio: 'inherit',
+  shell: true,
+});
+
+child.on('exit', (code) => {
+  process.exit(code ?? 0);
+});
